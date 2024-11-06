0a64d5b0c4b868fc18924542ee32d3dfddb4a9e7
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-11-06 15:39:24 -0300

    Implementado profile de produção, Refatorado projeto para obter persistência em banco de dados PostgreSQL.

--- src/main/java/com/todolist/todolist/Config/TestConfig.java
+++ src/main/java/com/todolist/todolist/Config/TestConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
+@Profile("prod")
 public class TestConfig implements CommandLineRunner {
 
     @Autowired
